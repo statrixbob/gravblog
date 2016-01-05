51db7b65b2bc8e26e8258dca948b32adc7b1e201
Robert Harrison <admin@statrix.org>
2016-01-05 00:21:04 -1000

    Renew git hooks

--- ___git_grabber.php
+++ ___git_grabber.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Pacific/Honolulu');
 ignore_user_abort(true);
 set_time_limit(0);
  
-$repo          = '~/apps/gravblog/public';
+$repo          = '~/www/html';
 $branch        = 'master';
 $output        = array();
  
